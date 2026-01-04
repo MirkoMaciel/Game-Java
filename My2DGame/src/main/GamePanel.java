23b456ea72eadede1677bae95061974596b6a755
MirkoMaciel <mirko.maciell@gmail.com>
2026-01-03 22:16:33 -0300

    Class Entity
    -Create a class "Entity" for the NPC, player, monsters

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -23,6 +23,8 @@ public class GamePanel extends JPanel implements Runnable { //Clase que hereda a
 	final int screenWidth = finalTile * maxScreenCol; //Tamaño del ancho de la ventana 768px
 	final int screenHeigth = finalTile * maxScreenRow; //Tamaño del alto de la ventana 576px
 	
+	//FPS
+	int FPS = 60;
 	
 	//KeyPress
 	KeyHandler keyH = new KeyHandler(); //Instancio la clase
@@ -57,7 +59,11 @@ public class GamePanel extends JPanel implements Runnable { //Clase que hereda a
 	public void run() {
 
 		while (gameThread != null) {
-			System.out.println("El juego está corriendo");
+
+			
+			//Draw
+			double drawInterval = 1000000000 / FPS; //0.016666 seconds
+			double nextDrawTime = System.nanoTime() + drawInterval;
 			
 			//1 : ACTUALIZAR
 			update();
@@ -65,12 +71,72 @@ public class GamePanel extends JPanel implements Runnable { //Clase que hereda a
 			//2 : DIBUJAR LA PANTALLA CON LA INFORMACIÓN ACTUALIZADA
 			repaint();
 			
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				remainingTime = remainingTime / 1000000; //Conversión de nanosegundos a milisegundos
+				if (remainingTime < 0 ) {
+					remainingTime = 0;
+				}
+				Thread.sleep( (long )remainingTime);
+				nextDrawTime += drawInterval;
+				
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 		}
 		
 	}
 	
+	/*
+	public void run () {
+		
+		double drawInterval = 1000000000 / FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
+		while (gameThread != null) {
+
+		currentTime = System.nanoTime();
+		
+		delta += (currentTime - lastTime) / drawInterval;
+		timer += (currentTime - lastTime);
+		lastTime = currentTime;
+		
+		if(delta >= 1){
+			update();
+			repaint();
+			delta--;
+			drawCount++;
+			
+		}
+		
+		if (timer >= 1000000000){
+			System.out.println("FPS:"+drawCount);
+			darwCount = 0;
+			timer =0;
+		}
+		
+		}
+	}*/
+	
 	public void update () {
 		
+		if (keyH.upPressed == true) {
+			playerY -=  playerSpeed;
+		}else if (keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}else if (keyH.leftPressed == true) {
+			playerX -=  playerSpeed;
+		}else if(keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -82,7 +148,7 @@ public class GamePanel extends JPanel implements Runnable { //Clase que hereda a
 		
 		g2.setColor(Color.white); //Seteo el color para dibujar objetos
 		
-		g2.fillRect(100, 100, finalTile, finalTile); //X,Y,WIDTH,HEIGTH parametros para dibujar, coordenadas X e Y y la altura y ancho del dibujo
+		g2.fillRect(playerX, playerY, finalTile, finalTile); //X,Y,WIDTH,HEIGTH parametros para dibujar, coordenadas X e Y y la altura y ancho del dibujo
 		
 		g2.dispose();
 	}
